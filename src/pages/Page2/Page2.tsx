ca0ea9f397c8db1c2d2506c0e1e39aab5a6caf79
Tomislav Smetko <tomislav.smetko@outlook.com>
2022-10-27 11:47:39 +0200

    dsfdsfds

--- src/pages/Page2/Page2.tsx
+++ src/pages/Page2/Page2.tsx
@@ -10,7 +10,7 @@ const Page2 = (propess:any)=>{
     const[displayList, setdisplayList] = useState(itemss);
 
     const toggle = ()=>{
-      console.log("..", list1)
+      console.log("jghjghj.", list1)
       if(list1){
         setdisplayList(itemss2);
       }else{
